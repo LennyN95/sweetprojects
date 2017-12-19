ff065aecbf69a4adbd8d3b3954cac458dc674c0c
Leonard Nuernberg <lenny@moontec.de>
2017-12-19 01:27:22 +0100

    package generator

---
+++ spec/sweetprojects-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import SweetprojectsView from '../lib/sweetprojects-view';
+
+describe('SweetprojectsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
